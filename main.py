263097fb493396955152d17b89774cbb7b365b89
alixvan <alixkamno400@gmail.com>
2026-07-06 16:19:08 +0200

    Création de la structure du projet

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("===================================")
+    print("   Mini Auditeur de Sécurité")
+    print("===================================")
+    print("Projet démarré avec succès !")
+
+
+if __name__ == "__main__":
+    main()
